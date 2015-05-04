23c07333e7256be51b337731098a69b2f26a9f38
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2015-05-04 09:59:40 -0400

    Initial working prototype

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600, "node-integration": false});
+
+  // and load the index.html of the app.
+  mainWindow.loadUrl('https://sandbox.adlnet.gov/966/adl/sandbox/example_sledgame/');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
